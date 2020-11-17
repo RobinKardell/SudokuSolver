7e168d62def28b2ae3c5b39daa12496dccecb2cf
RobinKardell <carden.rk@gmail.com>
2020-11-17 22:22:23 +0100

    Lägg till kommentarer i koden.

--- Program.cs
+++ Program.cs
@@ -22,21 +22,34 @@ namespace SudokuSolver
                 PrintSudoku(puzzle);
         }
         /*
-        PrintSudoku ritar ut plannen  
+        PrintSudoku ritar ut planen så det någonlunda liknar en sudoku plan.
         */
         public static void PrintSudoku(int[,] puzzle)
         {
+            //skriver ut första raden av 
             Console.WriteLine("+-----+-----+-----+");
 
+            //Loopar igenom alla 9 rader 
             for (int i = 1; i < 10; ++i)
             {
+                //Loopar igenom så det finns en avskiljare mellan talen. 
                 for (int j = 1; j < 10; ++j)
                     Console.Write("|{0}", puzzle[i - 1, j - 1]);
-
+                
+                //Denna lägger till den sista avskiljaren så det ramars in på ett snygg sätt.
                 Console.WriteLine("|");
+                
+                //Lägger till botten av spelplanen. 
                 if (i % 3 == 0) Console.WriteLine("+-----+-----+-----+");
             }
         }
 
+        /*
+         
+        */
+        public static bool SolveSudoku(int[,] puzzle, int row, int col)
+        {
+
+        }
     }
 }

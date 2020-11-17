45256a37601c808717b413661306948f250496e4
RobinKardell <carden.rk@gmail.com>
2020-11-17 23:34:22 +0100

    Här är en kommenterad och fungerad spelplan

--- Program.cs
+++ Program.cs
@@ -17,8 +17,9 @@ namespace SudokuSolver
                                 { 2, 0, 0, 0, 7, 0, 0, 1, 9 },
                                 { 0, 0, 0, 6, 0, 9, 5, 8, 2 }
                             };
-
-            //if (SolveSudoku(puzzle, 0, 0))
+            //om det kan lösas 
+            if (SolveSudoku(puzzle, 0, 0))
+                //skriv ut hela planen med lösningen så man kan visa upp det i konsolen.
                 PrintSudoku(puzzle);
         }
         /*
@@ -45,11 +46,69 @@ namespace SudokuSolver
         }
 
         /*
-         
+        Denna funktion ska göra själva lösningen på sudokun, 
         */
         public static bool SolveSudoku(int[,] puzzle, int row, int col)
         {
+            //först kolla så man inte gör mer än behvligt för att lösa sudokun. 
+            if (row < 9 && col < 9)
+            {
+                //om det är en siffra redan utpekad på spelplanen så behövs det inte kollas efter en anna siffra.
+                if (puzzle[row, col] != 0)
+                {
+                    if ((col + 1) < 9) return SolveSudoku(puzzle, row, col + 1);
+                    else if ((row + 1) < 9) return SolveSudoku(puzzle, row + 1, 0);
+                    else return true;
+                }
+                else
+                {
+                    //om det är en 0:a(nolla = tomt fält) på spelplanen så ska det fyllas med en fungerade siffra mellan 1-9,
+                    // men ändå vara unik 
+                    //i denna lopp går man igenom alla postioner och kollar om det är möjligt att sätta en fungerande siffra mellan 1-9 i rutan.
+                    for (int i = 0; i < 9; ++i)
+                    {
+                        if (IsAvailable(puzzle, row, col, i + 1))
+                        {
+                            puzzle[row, col] = i + 1;
 
+                            if ((col + 1) < 9)
+                            {
+                                if (SolveSudoku(puzzle, row, col + 1)) return true;
+                                else puzzle[row, col] = 0;
+                            }
+                            else if ((row + 1) < 9)
+                            {
+                                if (SolveSudoku(puzzle, row + 1, 0)) return true;
+                                else puzzle[row, col] = 0;
+                            }
+                            else return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+            else return true;
         }
+        /*
+        Här kollar vi om det är möjlgit att sätta en siffra i den angivna positionen. för i sudoku får det inte vara samma siffra inom 3x3 
+        vågrätt eler lodrätt på spelplannen som är 9x9.
+        */
+        private static bool IsAvailable(int[,] puzzle, int row, int col, int num)
+        {
+            int rowStart = (row / 3) * 3;
+            int colStart = (col / 3) * 3;
+
+            for (int i = 0; i < 9; ++i)
+            {
+                if (puzzle[row, i] == num) return false;
+                if (puzzle[i, col] == num) return false;
+                if (puzzle[rowStart + (i % 3), colStart + (i / 3)] == num) return false;
+            }
+
+            return true;
+        }
+
+
     }
 }
